107bb10a199f051d549b88992ce68128bb12eb48
JensenLyou <jassenlyou@foxmail.com>
2022-09-21 02:40:54 +0800

    feat:update layout styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,30 +1,37 @@
-import { Box, CssBaseline, Grid, styled } from "@mui/material";
+import { Box, CssBaseline, Grid, Stack, styled } from "@mui/material";
 import CommonApproveDialog from "./CommonApproveDialog";
 import SideBar from "./SideBar";
 
 const Main = styled("main")`
-  width: calc(100% - 280px);
+  /* width: calc(100% - 280px); */
   min-height: calc(100vh - 20px);
   padding: 20px;
-  margin: 20px 0 0 260px;
+  /* margin: 20px 0 0 260px; */
   background: #ff928d;
   border-radius: 12px 12px 0 0;
 `;
 
 const Layout = ({ children }) => {
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-      }}
+    <Stack
+      minWidth="1200px"
+      direction="row"
+      justifyContent="center"
+      sx={
+        {
+          // flexGrow: 1,
+        }
+      }
     >
       <CssBaseline />
       <Grid container>
         <Grid
           item
-          sx={{
-            position: "fixed",
-          }}
+          sx={
+            {
+              // position: "fixed",
+            }
+          }
         >
           <SideBar />
         </Grid>
@@ -34,7 +41,7 @@ const Layout = ({ children }) => {
       </Grid>
 
       <CommonApproveDialog />
-    </Box>
+    </Stack>
   );
 };
 
